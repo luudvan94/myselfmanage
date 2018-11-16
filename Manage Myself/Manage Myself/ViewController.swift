4e697ff451e32ae43b7f4c087f1eeafc41741555
Van Luu <valu4298@gmail.com>
2018-11-15 19:35:41 -0600

    Initialization project

---
+++ Manage Myself/Manage Myself/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Manage Myself
+//
+//  Created by Van Luu on 11/15/18.
+//  Copyright © 2018 Luu Dinh Van. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
